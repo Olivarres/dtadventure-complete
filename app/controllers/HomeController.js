1383506243a1a85b7a41e994a1944956b3894bb8
Hayet <marc.hayet@gmail.com>
2015-10-08 10:39:37 +0200

    Ajout tri par nom
    Ajout du tri par le nom dans la page Home

--- app/controllers/HomeController.js
+++ app/controllers/HomeController.js
@@ -1,8 +1,15 @@
 export default function HomeController (StepService) {
+    var homeCtrl = this;
 
     StepService.getSteps()
     .then(function (steps) {
         this.steps = steps
     }.bind(this))
+	
+	//methodes
+	homeCtrl.sortBy = function(attrib){
+		this.predicate = attrib;
+		this.reverse = !this.reverse
+	}
 
 }
