ff437b4ce6e29a0dfb0fd0354df853cf696dbffe
Hayet <marc.hayet@gmail.com>
2015-10-08 10:52:47 +0200

    MAJ du tri, Recherche et Filtre du Start
    Mise a jour du tri par ajout de valeurs initiales.
    Ajout d'un champ de recherche d'étapes.
    Ajout d'un filtre cachant le bouton 'start' sauf pour l'étape de
    démarrage.

--- app/controllers/HomeController.js
+++ app/controllers/HomeController.js
@@ -1,12 +1,15 @@
 export default function HomeController (StepService) {
     var homeCtrl = this;
-
+	
     StepService.getSteps()
     .then(function (steps) {
         this.steps = steps
     }.bind(this))
 	
-	//methodes
+	//valeur par defaut du tri
+	this.reverse = true;
+	this.predicate = "name";
+	
 	homeCtrl.sortBy = function(attrib){
 		this.predicate = attrib;
 		this.reverse = !this.reverse
